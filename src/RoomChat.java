09a3262172e99460feeb20834ec2a03d9519dd5e
Giordana Camargo <giordanacamilla@gmail.com>
2023-06-06 10:36:57 -0300

    Adição de funções na RoomChat.

--- src/RoomChat.java
+++ src/RoomChat.java
@@ -2,37 +2,36 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class RoomChat extends java.rmi.server.UnicastRemoteObject implements IRoomChat {
 
     private ArrayList<String> Users;
+    private Map<String, IUserChat> userList;
+
     private String roomName;
 
     public RoomChat(String roomName) throws RemoteException {
         this.roomName = roomName;
-        this.Users = new ArrayList<>();
+        this.userList = new HashMap<String, IUserChat>();
     }
 
-    public void sendMsg (String usrName, String msg) throws Exception {
-        Registry registry = LocateRegistry.getRegistry("localhost", 1099);
-        for (String userName: Users) {
-            IUserChat user = (IUserChat) registry.lookup(userName);
-            user.deliverMsg(usrName, msg);
+    public void sendMsg(String usrName, String msg) throws Exception {
+        for (Map.Entry<String, IUserChat> pair : userList.entrySet()) {
+            pair.getValue().deliverMsg(usrName, msg);
         }
     }
     public void joinRoom(String usrName, IUserChat user) {
-        this.Users.add(usrName);
+        userList.put(usrName, user);
     }
     public void leaveRoom(String usrName) {
-        this.Users.remove(usrName);
+        userList.remove(usrName);
     }
     public void closeRoom() {
         return;
     }
     public String getRoomName() {
         return roomName;
-    }
-    public void setRoomName (String roomName) {
-        this.roomName = roomName;
     }
 }
